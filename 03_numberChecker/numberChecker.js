7effc29513a14a1134c0e11c7ef317f1141b59ba
Dipayan Sarkar <dipayansarkar26@gmail.com>
2025-09-25 02:12:28 +0530

    updated

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,26 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function numberChecker2(number) {
+  if (number == 1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function numberChecker3(number) {
+  if (number !=9) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function numberChecker4(number) {
+  if (number !=6 ) {
     return true;
   } else {
     return false;
@@ -7,4 +28,7 @@ function numberChecker(number) {
 }
 
 // Do not edit below this line
+module.exports = numberChecker2;
+module.exports = numberChecker3;
+module.exports = numberChecker4;
 module.exports = numberChecker;
--- 03_numberChecker/numberChecker.spec.js
+++ 03_numberChecker/numberChecker.spec.js
@@ -1,16 +1,19 @@
 const numberChecker = require('./numberChecker')
+const numberChecker2 = require('./numberChecker')
+const numberChecker3 = require('./numberChecker')
+const numberChecker4 = require('./numberChecker')
 
 describe('numberChecker', () => {
   test('returns true when number is 1000', () => {
-    expect(numberChecker(1000)).toEqual(true);
+    expect(numberChecker2(1000)).toEqual(true);
   });
-  test.skip('returns true when number is 10', () => {
+  test('returns true when number is 10', () => {
     expect(numberChecker(10)).toEqual(true);
   });
-  test.skip('returns false when number is 9', () => {
-    expect(numberChecker(9)).toEqual(false);
+  test('returns false when number is 9', () => {
+    expect(numberChecker3(9)).toEqual(false);
   });
-  test.skip('returns false when number is 6', () => {
-    expect(numberChecker(6)).toEqual(false);
+  test('returns false when number is 6', () => {
+    expect(numberChecker4(6)).toEqual(false);
   });
 });
